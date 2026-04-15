ffdecde7b9826cd9ae4cadb730071ed63858d6ab
umifly <87199356+umifly@users.noreply.github.com>
2026-04-15 12:53:42 +0800

    Update luban.sh
    Add a function for sparse cloning Git repositories and update versioning.

--- luban.sh
+++ luban.sh
@@ -1,1 +1,21 @@
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
 
+# iStore
+git_sparse_clone main https://github.com/kiddin9/op-packages luci-app-quickstart
+git_sparse_clone main https://github.com/kiddin9/op-packages quickstart
+
+# 修改版本为编译日期
+date_version=$(date +"%y.%m.%d")
+orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+sed -i "s/${orig_version}/R${date_version} by umifly/g" package/lean/default-settings/files/zzz-default-settings
+
+./scripts/feeds update -a
+./scripts/feeds install -a
